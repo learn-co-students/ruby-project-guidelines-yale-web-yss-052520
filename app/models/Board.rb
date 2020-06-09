87cedf0b25f20fbfc9791d73ca274698c2db25af
KalebGz <kaleb.zeray@gmail.com>
2020-06-09 19:20:51 -0400

    added #load and #display to board

--- app/models/Board.rb
+++ app/models/Board.rb
@@ -3,4 +3,32 @@ class Board < ActiveRecord::Base # instances of this class are stored in the boa
     belongs_to :l_player, :class_name => 'Player', :foreign_key => 'l_player_id'
     belongs_to :r_player, :class_name => 'Player', :foreign_key => 'r_player_id'
     
+    # Creates the pieces associatied with the current board configuration
+    def load
+        grid = contents.split("\n")
+        grid.length.times{|row|
+            grid[row].length.times{|col|
+                if grid[row][col] == 🔴
+                    Piece.new(row, col, "right", 🔴)
+                else grid[row][col] == 🔵
+                    Piece.new(row, col, "left", 🔵)
+        self.display
+    end
+
+    # Prints out the current configuration of this board's content
+    def display        
+        # TODO: Find Gem to clear the CLI screen
+        p self.content
+        if(self.player_turn == 'l')
+            p "It is #{Player.find(self.l_player_id)}'s turn!'"
+        else
+            p "It is #{Player.find(self.r_player_id)}'s turn!'"
+        end
+    end
+
+    def update
+        
+
+    end
+
 end
