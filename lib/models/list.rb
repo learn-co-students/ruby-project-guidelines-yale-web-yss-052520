139f377b3bbef2d157c584d263382ea7bd230eb7
s-dney <littlebirdsword@gmail.com>
2020-06-09 07:31:05 -0700

    added find author by full name

--- lib/models/list.rb
+++ lib/models/list.rb
@@ -3,9 +3,6 @@ class List < ActiveRecord::Base
   has_many :entries
   has_many :books, through: :entries
 
-  
-
-
 
     
 end
@@ -16,4 +13,10 @@ end
 
 def parse_last_name(fullname)
   fullname.partition(" ").last
+end
+
+def find_author_by_full_name(fullname)
+  first = parse_first_name(fullname)
+  last = parse_last_name(fullname)
+  Author.where(first_name: first, last_name: last)
 end
