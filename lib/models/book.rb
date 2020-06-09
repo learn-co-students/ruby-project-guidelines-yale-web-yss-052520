e982835814083cf4a4806934288924e99b31a8e1
s-dney <littlebirdsword@gmail.com>
2020-06-09 10:22:13 -0700

    made mark as read method, changed read? column to read because it was causing issues

--- lib/models/book.rb
+++ lib/models/book.rb
@@ -3,5 +3,13 @@ class Book < ActiveRecord::Base
   belongs_to :author
   has_many :entries
   has_many :lists, through: :entries
+
+  def mark_as_read
+    self.read = true
+  end
+
+  def set_genre(genre)
+    self.genre = genre
+  end
     
 end
