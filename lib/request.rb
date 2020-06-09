9c820de17483db8ab24be3d22b03112ce8e26be3
chad <chad.palmer@yale.edu>
2020-06-09 14:24:18 -0400

    added empty migration, information in models

--- lib/request.rb
+++ lib/request.rb
@@ -1,4 +1,10 @@
 class Request
     attr_accessor :api, :locations, :dates, :case_type
 
+    def initialize(args)
+        @api = args[:api]
+        @locations = args[:locations]
+        @dates = args[:dates]
+        @case_type = args[:case_type]
+    end
 end
