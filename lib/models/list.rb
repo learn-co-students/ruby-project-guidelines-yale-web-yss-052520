51f31ca033538efde5c2a8f1063070d0f4e9ec2e
s-dney <littlebirdsword@gmail.com>
2020-06-09 09:33:44 -0700

    finished add book to list method; moved create_entry to private

--- lib/models/list.rb
+++ lib/models/list.rb
@@ -3,10 +3,6 @@ class List < ActiveRecord::Base
   has_many :entries
   has_many :books, through: :entries
 
-  def create_entry(book)
-    Entry.create(list_id: self.id, book_id: book.id)
-  end
-
   
   def add_book_to_list(title:, authorname:, year: nil, genre: nil)
     author = find_or_create_author_by_full_name(authorname)
@@ -16,7 +12,13 @@ class List < ActiveRecord::Base
       newbook = Book.create(title: title, author_id: author.id, year: year, genre: genre)
       create_entry(newbook)
     end
+    self.reload
+  end
 
+  private
+
+  def create_entry(book)
+    Entry.create(list_id: self.id, book_id: book.id)
   end
     
 end
