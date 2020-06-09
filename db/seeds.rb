dc98567c36738ed1c24a7d019a42673ce8b7dc31
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-09 14:15:52 -0400

    tables created with examples

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+User.destroy_all 
+Official.destroy_all
+Email.destroy_all 
+User.reset_pk_sequence
+Official.reset_pk_sequence
+Email.reset_pk_sequence
+
+user1 = User.create(name: "Anmol", address: "address1", comment: "comment1")
+user2 = User.create(name: "Larissa", address: "address2", comment: "comment2")
+
+official1 = Official.create(name: "John", location: "Queens, NY", email: "email1@gmail.com", role: "State Rep")
+official2 = Official.create(name: "Hannah", location: "Princeton, NJ", email: "email2@gmail.com", role: "Local Rep")
+
+Email.create(message: "body", subject: "BLM", link: "link.com", user_id: user1.id, official_id: official1.id)
+Email.create(message: "body2", subject: "Police brtality", link: "link2.com", user_id: user2.id, official_id: official2.id)
+
+
+binding.pry
+0
