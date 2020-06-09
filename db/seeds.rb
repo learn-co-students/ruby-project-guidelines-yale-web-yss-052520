daf73bfdbafcbe10b83e7f029a47270d3d0aca69
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-09 16:05:49 -0400

    testing in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,9 @@ official2 = Official.create(name: "Hannah", location: "Princeton, NJ", email: "e
 Email.create(message: "body", subject: "BLM", link: "link.com", user_id: user1.id, official_id: official1.id)
 Email.create(message: "body2", subject: "Police brtality", link: "link2.com", user_id: user2.id, official_id: official2.id)
 
-url = "https://www.googleapis.com/civicinfo/v2/representatives"
+API_key = "AIzaSyDKhxw94NRo5C5xpO1_3Sg5dfZvxjZmF3Y"
+address = "9586%20112th%20Street%2C%20South%20Richmond%20Hill%2C%20NY%2C%2011419"
+url = "https://www.googleapis.com/civicinfo/v2/representatives?address=#{address}&key=#{API_key}"
 data = HTTParty.get(url)
 
 binding.pry
