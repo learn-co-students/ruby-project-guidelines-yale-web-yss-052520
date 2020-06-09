a84b9c1328f954da965e8c2827453f17f99945fe
larissanguyen <larissa.h.nguyen@gmail.com>
2020-06-09 15:20:23 -0400

    env api key

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,13 @@
-require_relative '../config/environment'
+# require_relative '../config/environment'
+require 'net/http'
 
+url = URI.parse('https://www.googleapis.com/civicinfo/v2/representatives')
+req = Net::HTTP::Get.new(url.to_s)
+# res = Net::HTTP.start(url.host, url.port) {|http|
+#   http.request(req)
+# }
+pp req
 
 
 puts "HELLO WORLD"
+
