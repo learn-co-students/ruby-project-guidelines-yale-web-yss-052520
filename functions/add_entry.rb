2c5505612e449b9c02abc1ff8392786a41491a30
arthur318 <arthurhwang318@gmail.com>
2020-06-10 12:14:07 -0700

    updated add entry function

--- functions/add_entry.rb
+++ functions/add_entry.rb
@@ -1,13 +1,26 @@
 require_relative '../config/environment'
+$prompt = TTY::Prompt.new
 
-def add_entry
+def add_entry(current_list)
     system("clear")
     puts "Add New Book"
     puts "-"*100
     sleep(0.5)
 
-    
+    entry = $prompt.collect do
+        key(:title).ask('Title?', required: true)
+      
+        key(:authorname).ask('Author?', required: true)
+      
+        key(:year).ask('Year? (optional)', default: nil)
+        key(:genre).ask('Genre? (optional)', default: nil)
+      end
 
-
+    current_list.add_book_to_list(entry)
 
 end
+
+# cujo = Book.create(title: "Cujo", author_id: king.id, year: 1981, genre: "horror", format: "book")
+# def add_book_to_list(title:, authorname:, year: nil, genre: nil)
+
+# add_entry
--- functions/view_all.rb
+++ functions/view_all.rb
@@ -5,18 +5,21 @@ def view_all
     puts "My Lists"
     puts "-"*100
     sleep(0.5)
-
-    array1 = List.all.map do |list| 
-        list.name 
-    end
-
-    array2 = List.all
-    hash = array1.zip(array2).to_h
     # array = List.select(:name)
-    selected = $prompt.select("Select list:", hash)
+    selected = $prompt.select("Select list:", list_hash)
     puts selected
     # view_list(selected)
 end
+
+#helper method
+def list_hash
+    array1 = List.all.map do |list| 
+        list.name 
+    end
+    array2 = List.all
+    hash = array1.zip(array2).to_h
+end
+
 #iterate and make a menu into a list?
 
 #pass into the selected menu into view_list(as an arg)
