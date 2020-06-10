329704c98434757a6f5f6427de0bafc70f4f5ccf
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-10 17:10:57 -0400

    adding line into env

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,6 @@ Bundler.require
 ActiveRecord::Base.establish_connection(
     adapter: 'sqlite3', 
     database: 'db/development.db')
+ActiveRecord::Base.logger = nil    
 require_all 'lib'
 require_all 'app'
