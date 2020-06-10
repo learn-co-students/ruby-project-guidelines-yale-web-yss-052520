bdc5236fe0cd6cdbe0485244360f21623a3769fe
justin_james <jjames11799@gmail.com>
2020-06-09 21:34:48 -0400

    Tuesday work

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,4 +1,34 @@
 class ToDo < ActiveRecord::Base
     belongs_to :task
     belongs_to :user
+
+    def self.get_priority
+        priority = ToDo.where(priority_level: 5)
+        priority << ToDo.where(task.due_date <=> Date.today.next = 0)
+        priority << ToDo.where(task.due_date <=> Date.today = 0)
+        priority << Todo.where(task.due_date <=> Date.today = -1)
+        priority.flatten.destroy_by(complete?: true)
+        priority 
+    end
+
+    def self.all_incomplete_tasks
+        all_incomoplete_tasks = ToDo.where(complete?: false) 
+    end
+
+    def self.all_complete_tasks
+        all_complete_tasks = ToDo.where(complete?: true)
+    end
+
+    def delete_specified_todo
+        self.destroy
+    end
+
+    def self.destroy_all_complete_tasks
+        ToDo.destroy_by(complete?: true)
+    end
+# To make this method work, from the command line, we want the user to input name, college and age for user instance, throw in category, and relevant details for task
+# Will likely want to structure into a hash prior to passing into make_to
+    def make_todo(attributes)
+        ToDo.create(attributes)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
     has_many :to_dos
     has_many :tasks, through: :to_dos
+
+    def self.make_new_user(attributes)
+        User.find_or_create_by(attributes)
+    end
 end
