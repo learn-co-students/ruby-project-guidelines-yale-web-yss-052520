dccb5ef56f9e6ee77af01d6d00b98d5a243c20e0
davidyu7 <david.yu@yale.edu>
2020-06-11 12:52:32 -0400

    implemented execute_move and validate_move

--- checkers.rb
+++ checkers.rb
@@ -42,3 +42,68 @@ game.load
 
 binding.pry
 0
+
+# returns move type as a string if valid, nil if invalid
+def validate_move(piece, to_pos)
+    if piece.regular_moves.include?(to_pos) # if its a regular move
+        return "regular"
+    elsif piece.jump_moves.include?(to_pos) # if its a jump move
+        return "jump"
+    else
+        return nil
+    end
+end
+
+def execute_move(piece, move_type, to_pos)
+    # parses destination coordinate array into individual x and y values
+    to_x = to_pos[0]
+    to_y = to_pos[1]
+
+    if move_type == "regular"
+        # set the piece to its new position
+        piece.x_pos = to_x
+        piece.y_pos = to_y
+        return true # move is complete
+
+    elsif move_type == "jump"
+        loop do # runs until all jump moves have been exhausted
+            # remembers the starting position of the piece
+            from_x = piece.x_pos
+            from_y = piece.y_pos
+
+            piece.x_pos = to_x
+            piece.y_pos = to_y
+
+            # finds captured piece and removes it from the piece.all array - ruby will garbage collect
+            Piece.all.delete(Piece.all.find{|p| p.x_pos == (from_x + to_x)/2 && p.y_pox == (from_y + to_y)/2})
+
+            # recalculates posisble jump moves and checks if there are any left
+            if piece.jump_moves.empty?
+                return true # move is complete
+            else
+                loop do # runs until the user provides a valid jump move destination
+                    jump_input = gets("Please provide the coordinates of your next jump.")
+
+                    if /^[A-Ha-h][1-8]$/.match(input) # the user has provided a coordinate in correct format
+                        to_pos = input.split("") # splits input string into an array [x,y]
+                        to_pos[0] = to_pos[0].to_i # converts letter x-coord to number
+
+                        if piece.jump_moves.include?(to_pos) # if the user input coordinate is indeed a jump move
+                            # parses destination coordinate array into individual x and y values
+                            to_x = to_pos[0]
+                            to_y = to_pos[1]
+
+                            break # exits into main loop to execute another jump move
+                        else # if the user input coordinate is not a jump move
+                            puts "You must jump over an opponent piece."
+                        end
+                    else # the user has not provided a coordinate in wrong format
+                        puts "That is not a valid coordinate. Examples: 'A2', 'g5'"
+                    end
+                end
+            end
+        end
+    end
+end
+
+
