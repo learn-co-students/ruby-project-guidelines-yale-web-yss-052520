010642e646177c8ee3a04753f92a2603495a306f
ZIming Mao <ziming.mao@yale.edu>
2020-06-11 11:54:29 -0400

    export graphs and data to respective folders

--- bin/run.rb
+++ bin/run.rb
@@ -1,20 +1,17 @@
 require_relative '../config/environment'
 
-# Things finished tonight:
-# Graph (support multiple country comparison) and Txt exports
-# Multiple countries output (support both graph export and txt export)
-# Record case types in graph and text outputs
-# Added 'Quit' option for some $prompt.select
-# Added "Increment" option to both graph and text outputs
+# Finished by 12pm 
+# The earliest date with data is Jan 22th  (done)
+# Export data to the exports/data folder and label the file as "id_1_confirmed_cases_daily.txt" (done)
+# Export graph to the exports/graph folder and label the file as "id_1_confirmed_cases_daily_graph.txt" (done)
+# Create folder if not already existed (done)
+# The date must be in correct format DD/MM, e.g. MM is between 1 to 12 (done)
 
-# Edge cases:
-# Check whether the API has the country data
-# Date entered is later than the current date 
-# Starting date must be earlier than the ending date
-
-# Question:
-# Do we need to keep track of multiple queries? Each query already supports multiple countries and time durations.
-# We should try to finish everything by Thursday. Also there is video and documentation to write.
+# To-do-list
+# Change the display message to warnings, use $prompt to make everything look nicer
+# Fix the graph percentage label, round sometimes gives "7.0000001", so I used string truncation, that still gives a problem: 100.%. 
+# Make sure that at each step there is an option "Quit", so that user can end the app early
+# Write a good README file, such as "bundle install", run "rake db:migrate", and graphs are exported to 'exports/graphs' and data are exported to 'exports/data' folders
 
 def run
     Seeder.clear
@@ -99,22 +96,37 @@ end
 
 def date_prompt
     query = Query.all.last
-    selection = $prompt.select('Would you like to search by a date or a range of dates?', {'Date' => 0, 'Range of dates' => 1})
+    selection = $prompt.select('Would you like to search by a date or a range of dates?', {'Date' => 0, 'Range of dates' => 1, 'Quit' => 2})
 
     case selection
     when 0
         date = $prompt.ask("Provide a date (DD/MM):") {|q| q.validate(/[0-9][0-9]\/[0-9][0-9]/,'Please format as DD/MM')}
+
+        if !Date.valid_date? 2020,date[3..4].to_i, date[0..1].to_i #check whether it is a valid date
+            puts "Date input incorrect"
+            date_prompt
+            return 0
+        end
+
         if check_date(date) == false
             puts "Selected date cannot be later than the current date!"
             date_prompt
             return 0
         end
+
         query.date_type = "single"
         date_array = Query.create_date_array(date)
         query.single_date = date_array
     when 1
         starting_date = $prompt.ask("Provide a starting date (DD/MM):") {|q| q.validate(/[0-9][0-9]\/[0-9][0-9]/, 'Please format as DD/MM')}
         ending_date = $prompt.ask("Provide a ending date (DD/MM):") {|q| q.validate(/[0-9][0-9]\/[0-9][0-9]/, 'Please format as DD/MM')}
+
+        if !Date.valid_date? 2020,starting_date[3..4].to_i, starting_date[0..1].to_i || !Date.valid_date? 2020,ending_date[3..4].to_i, ending_date[0..1].to_i
+            puts "Date input incorrect"
+            date_prompt
+            return 0
+        end
+
         if check_date(starting_date) == false || check_date(ending_date) == false
             puts "Selected date cannot be later than the current date!"
             date_prompt
@@ -125,12 +137,19 @@ def date_prompt
             date_prompt
             return 0
         end
+        if check_date_available(starting_date) == false || check_date_available(ending_date) == false
+            puts "Data are only available for dates later than Jan 22 "
+            date_prompt
+            return 0
+        end
         query.date_type = "range"
         starting_date_array = Query.create_date_array(starting_date)
         ending_date_array = Query.create_date_array(ending_date)
 
         query.starting_date = starting_date_array
         query.ending_date = ending_date_array
+    when 2
+        exit
     end
 end
 
@@ -145,15 +164,15 @@ def export_prompt
     
     case selection
     when 1
-        Exporter.export_txt(data)
+        Exporter.export_txt(data, "daily")
     when 2
         data_incremented = Query.increment(data)
-        Exporter.export_txt(data_incremented)
+        Exporter.export_txt(data_incremented, "cumulative")
     when 3
-        Exporter.graph(data)
+        Exporter.graph(data, "daily")
     when 4
         data_incremented = Query.increment(data)
-        Exporter.graph(data_incremented)
+        Exporter.graph(data_incremented, "cumulative")
     when 5
         puts "Returning to menu..."
     end
@@ -172,7 +191,8 @@ def seed_database(country_slug)
     end
 end
 
-def check_date(date1, date2 = Time.now.strftime("%d/%m/%Y %H:%M"))
+def check_date(date1, date2 = Time.now.strftime("%d/%m/%Y %H:%M"))    
+    
     # time_now = Time.now.strftime("%d/%m/%Y %H:%M")
     #=> "14/09/2011 14:09"
     # date => DD/MM
@@ -186,4 +206,17 @@ def check_date(date1, date2 = Time.now.strftime("%d/%m/%Y %H:%M"))
     end
 end
 
+def check_date_available(date)
+    # DD/MM
+
+    if date[3..4].to_i == 1
+        if date[1..2].to_i >= 22
+            true
+        else
+            false
+        end
+    else 
+        true
+    end
+end
 run
--- lib/exporter.rb
+++ lib/exporter.rb
@@ -2,8 +2,8 @@
 
 class Exporter
 
-    def self.graph(input)
-        puts ("This is a graph for " + Query.load.case_type)
+    def self.graph(input, text)
+        puts ("This is a graph for " + Query.load.case_type + " " + text)
         max = 0.00
         input.each do |arr|
             if arr[2] > max
@@ -30,13 +30,61 @@ class Exporter
             print " #{(percentage*100).to_s[0..3]}% - #{arr[2]} cases"
             puts ""
         end
+
+        # User can choose whether to export this graph to a txt file
+        selection = $prompt.select("Would you like to export this graph to a file", ["yes", "no"])
+        case selection
+        when "yes"
+            self.export_graph(input, text)
+        when "no"
+        end
     end
     
-    def self.export_txt(input)
-        puts("File is going to be exported to the current folder")
-        out_file = File.new("data.txt", "w")
+    def self.export_graph(input, text)
+        FileUtils.mkdir_p 'exports/graphs' 
+        # This Gem is used to create folder if not already existed
+        puts("File is going to be exported to the graph folder")
+        out_file = File.new("exports/graphs/id_#{Query.load.id.to_s}_#{Query.load.case_type}_#{text}_graph.
+        txt", "w")
+
+        out_file.puts ("This is a graph for " + Query.load.case_type + " " + text)
+        max = 0.00
+        input.each do |arr|
+            if arr[2] > max
+                max = arr[2].to_f
+            end
+        end
+        base = 60.00
         country_name = ""
-        out_file.puts ("This is a chart for " + Query.load.case_type)
+        input.each do |arr|
+            if country_name != arr[0]
+                country_name = arr[0]
+                out_file.puts("")
+                out_file.puts("-----------------------")
+                out_file.puts("The graph for " + country_name + " (from: " + input[0][1].to_s[0..9] + " to: " + input[-1][1].to_s[0..9] + ")")
+                out_file.puts ("----------------------")
+                out_file.puts("")
+            end
+            percentage = (arr[2]/max).round(2)
+            out_file.print "#{arr[1].to_s[0..9]}: "
+            number_of_stars = percentage*base.round
+            for i in 1..number_of_stars do
+                out_file.print "*"
+            end
+            out_file.print " #{(percentage*100).to_s[0..3]}% - #{arr[2]} cases"
+            out_file.puts ""
+        end
+        out_file.close
+        puts "Done! File has been created"
+
+    end
+
+    def self.export_txt(input, text)
+        FileUtils.mkdir_p 'exports/data'
+        puts("File is going to be exported to the data folder")
+        out_file = File.new("exports/data/id_#{Query.load.id.to_s}_#{Query.load.case_type}_#{text}.txt", "w")
+        country_name = ""
+        out_file.puts ("This is a chart for " + Query.load.case_type + " " + text)
 
         input.each do |arr|
             if country_name != arr[0]
@@ -50,6 +98,6 @@ class Exporter
             out_file.puts("Date: " + arr[1].to_s[0..9] + " Cases: " + arr[2].to_s)
         end
         out_file.close
-        puts "Done! File data.txt has been created"
+        puts "Done! File has been created"
     end
 end
